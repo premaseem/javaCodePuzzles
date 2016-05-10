04ea22c46dfb0ded530ccd8cf17ec8d7fe88b7e0
Aseem Jain <Aseem.Jain@rackspace.com>
2016-05-10 18:08:09 -0500

    optimized logic fo wiered number finder

--- src/javaCodePuzzles/com/premaseem/codechallange/FindWieredNumber.java
+++ src/javaCodePuzzles/com/premaseem/codechallange/FindWieredNumber.java
@@ -22,13 +22,12 @@ public class FindWieredNumber {
         // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
 
 
-        if (n % 2 == 1) {
+        if(n%2==1){
             ans = "Weird";
-        } else if (n >= 2 || n <= 5) {
+        }
+        else if(n >= 2 || n <= 5){
             ans = "Not Weird";
-        } else if (n >= 6 || n <= 20) {
-            ans = "Weird";
-        } else if (n > 20) {
+        }else if(n >= 6 ){
             ans = "Weird";
         }
         System.out.println(ans);
